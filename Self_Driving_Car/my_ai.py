7003726cedbe8b1f14b5e023e613a7707e777767
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2017-12-25 03:11:34 -0200

    start self driving car

---
+++ Self_Driving_Car/my_ai.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 24 12:42:14 2017
+
+@author: pedro
+"""
+
+# AI for Self Driving Car
+
+# Importing the libraries
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as auto
+from torch.autograd import Variable
+
+# Creating the architeture of the neural network
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+        
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+    
+# Implementing experience replay
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+            
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
